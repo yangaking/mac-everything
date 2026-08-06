ccae079a3daef7d2f322bb5c967797e409852280
yang <yang@aking.com>
2026-08-06 10:35:00 +0800

    chore: bump version to 0.1.1

--- build.sh
+++ build.sh
@@ -39,9 +39,9 @@ cat << 'EOF' > build/MacEverything.app/Contents/Info.plist
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.1</string>
+	<string>0.1.1</string>
 	<key>CFBundleVersion</key>
-	<string>1</string>
+	<string>2</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>12.0</string>
 	<key>NSHighResolutionCapable</key>
