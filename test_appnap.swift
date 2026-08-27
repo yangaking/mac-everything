7bdeadcf35b8825e6768936fa3ef9c3805d5888d
yang <yang@aking.com>
2026-08-27 21:43:24 +0800

    chore: remove scratch files

--- test_appnap.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-let token = ProcessInfo.processInfo.beginActivity(
-    options: [.userInitiatedAllowingIdleSystemSleep, .latencyCritical],
-    reason: "test"
-)
-print("Success")
